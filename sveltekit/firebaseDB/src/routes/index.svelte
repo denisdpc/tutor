45acc45940893384e1068c213a92896447ce22b6
nacionalizacao <nnac.celog@gmail.com>
2021-11-29 16:15:10 -0200

    todos de array

--- sveltekit/firebaseDB/src/routes/index.svelte
+++ sveltekit/firebaseDB/src/routes/index.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let todos = [];
+    let task = "";
+   
+    const addTodo = () => {
+        let todo = {
+            task: task,
+            isComplete: false,
+            createdAt: new Date(),
+        };
+        todos = [todo, ...todos];
+        task = "";
+    }
+
+    const markAsComplete = (index) => {        
+        todos[index].isComplete = !todos[index].isComplete;
+    }
+
+    const deleteTodo = (index) => {
+        let deleteItem = todos[index];
+        todos = todos.filter((item) => item != deleteItem);
+    }
+
+    $: console.table(todos);
+
+</script>
+
+<input type="text" placeholder = "Add a task" bind:value={task}>
+<button on:click={addTodo}>Add</button>
+
+
+<ol>
+    {#each todos as todo, index}
+        <li>
+            <span class:complete={todo.isComplete}>{todo.task}</span>
+            <span><button on:click={() => markAsComplete(index)}>✔</button></span>
+            <span><button on:click={() => deleteTodo(index)}>✘</button></span>
+        </li>
+    {:else}
+        No todos found
+    {/each}
+</ol>
+
+
+<style>
+    .complete {
+        text-decoration: line-through;
+    }
+</style>
+
