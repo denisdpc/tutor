7448183404f154fe06d2713512947b454b05b715
nacionalizacao <nnac.celog@gmail.com>
2021-12-01 15:13:37 -0200

    update delete

--- sveltekit/firebaseDB/src/routes/index.svelte
+++ sveltekit/firebaseDB/src/routes/index.svelte
@@ -1,13 +1,11 @@
 <script context="module">
     import { db } from "$lib/firebaseConfig";    
-    import { collection, query, addDoc, getDocs } from "firebase/firestore";
+    import { collection, query, addDoc, deleteDoc, updateDoc, getDocs, doc, DocumentReference } from "firebase/firestore";
 </script>
 
 <script>
     import { todosStore } from "$lib/stores";
     import { onMount } from "svelte";
-    
-    let todos = $todosStore;   
 
     onMount(async() => {
         if ($todosStore.length === 0) {
@@ -18,7 +16,7 @@
                 let todo = {...doc.data(), id: doc.id};
                 $todosStore.push(todo)
             });
-            todos = $todosStore;
+            $todosStore = $todosStore;
         }        
     });
     
@@ -33,18 +31,23 @@
         await addDoc(collection(db, "todos"), {
             todo: newTodo
         });
-        
-        todos = [newTodo, ...todos];
+
+        $todosStore = [newTodo, ...todos];
         task = "";
     }
 
-    const markAsComplete = (index) => {        
-        todos[index].isComplete = !todos[index].isComplete;
+    const deleteTodo = async(index) => {
+        let item = $todosStore[index];
+        await deleteDoc(doc(db,"todos", item.id));
+        $todosStore = $todosStore.filter((itemList) => itemList != item);
     }
 
-    const deleteTodo = (index) => {
-        let deleteItem = todos[index];
-        todos = todos.filter((item) => item != deleteItem);
+    const markAsComplete = async(index) => {
+        let item = $todosStore[index];
+        await updateDoc(doc(db,"todos",item.id), {
+            isComplete: !item.isComplete
+        });        
+        $todosStore[index].isComplete = !$todosStore[index].isComplete;
     }
 
     const keyIsPressed = (event) => {
@@ -53,16 +56,13 @@
         }
     }
 
-
-    //$: console.table(todos);
-
 </script>
 
 <input type="text" placeholder = "Add a task" bind:value={task} on:keydown={keyIsPressed}>
 <button on:click={addTodo}>Add</button>
 
-<ol>
-    {#each todos as todo, index}
+<ol>    
+    {#each $todosStore as todo, index}
         <li>
             <span class:complete={todo.isComplete}>{todo.task}</span>
             <span><button on:click={() => markAsComplete(index)}>✔</button></span>
@@ -72,9 +72,6 @@
         No todos found
     {/each}
 </ol>
-
-
-<!-- <svelte:window on:keydown={keyIsPressed}></svelte:window> -->
 
 <style>
     .complete {
