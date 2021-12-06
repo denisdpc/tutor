06daea2bb9423bdb1bb4e8e5907759f387440e5b
nacionalizacao <nnac.celog@gmail.com>
2021-12-06 11:12:54 -0200

    menu

--- sveltekit/visual/src/routes/index.svelte
+++ sveltekit/visual/src/routes/index.svelte
@@ -1,17 +1,40 @@
-<div class="rounded-lg shadow bg-base-200 drawer h-52">
-  <input id="my-drawer" type="checkbox" class="drawer-toggle"> 
-  <div class="flex flex-col items-center justify-center drawer-content">
-    <label for="my-drawer" class="btn btn-primary drawer-button">open menu</label>
-  </div> 
-  <div class="drawer-side">
-    <label for="my-drawer" class="drawer-overlay"></label> 
-    <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-      <li>
-        <a>Menu Item</a>
-      </li> 
-      <li>
-        <a>Menu Item</a>
-      </li>
-    </ul>
+<div class="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box">
+  <div class="flex-none px-2 mx-2">
+    <span class="text-lg font-bold">App</span>
   </div>
+
+  <div class="flex-1 px-2 mx-2">
+    <div class="items-stretch hidden lg:flex">
+      <!-- svelte-ignore a11y-missing-attribute -->
+      <a class="btn btn-ghost btn-sm rounded-btn">
+        Home
+      </a> 
+      <!-- svelte-ignore a11y-missing-attribute -->
+      <a class="btn btn-ghost btn-sm rounded-btn">
+        Portfolio
+      </a> 
+      <!-- svelte-ignore a11y-missing-attribute -->
+      <a class="btn btn-ghost btn-sm rounded-btn">
+        About
+      </a> 
+      <!-- svelte-ignore a11y-missing-attribute -->
+      <a class="btn btn-ghost btn-sm rounded-btn">
+        Contact
+      </a>
+    </div>
+  </div> 
+
+  <div class="flex-none">        
+    <div class="avatar">
+      <div class="rounded-full w-10 h-10 m-1">
+        <img src="https://i.pravatar.cc/500?img=32">
+      </div>
+    </div>
+  </div> 
+
+  <!-- svelte-ignore a11y-missing-attribute -->
+  <a class="btn btn-ghost btn-sm rounded-btn">
+    logout
+  </a>
+
 </div>
