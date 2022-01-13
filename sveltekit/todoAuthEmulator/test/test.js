9c88ad8aaa1c36b734acc3f71209bc5aefedcde7
denisdpc <denisdpc@gmail.com>
2022-01-13 09:18:03 -0300

    teste escrita

--- sveltekit/todoAuthEmulator/test/test.js
+++ sveltekit/todoAuthEmulator/test/test.js
@@ -20,4 +20,13 @@ describe('Teste INICIAL', () => {
         await firebase.assertFails(testDoc.set({foo:"bar"}));
     });
 
+    it("Can't write to a user document with the same ID as our user", async() => {
+        const db = firebase.initializeTestApp({projectId:MY_PROJECT_ID}).firestore();
+        const testDoc = db.collection("users").doc("user_abc");
+        await firebase.assertSucceeds(testDoc.set({foo:"bar"}));
+    });
+
+    // TODO: 16:50
+    // https://www.youtube.com/watch?v=VDulvfBpzZE&t=476s
+
 });
