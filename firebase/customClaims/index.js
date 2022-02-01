55aba1ff429aef4c345e0d9080d231d5180153f3
denis <denisdpc@gmail.com>
2022-02-01 14:23:56 +0000

    getAuth

--- firebase/customClaims/index.js
+++ firebase/customClaims/index.js
@@ -7,9 +7,13 @@ const app = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
+const getAuth = () => {
+  return app.auth();
+}
+
 const listAllUsers = (nextPageToken) => {
      // List batch of users, 1000 at a time.
-    app.auth()
+    getAuth()
        .listUsers(1000, nextPageToken)
        .then((listUsersResult) => {
          listUsersResult.users.forEach((userRecord) => {
@@ -24,36 +28,5 @@ const listAllUsers = (nextPageToken) => {
          console.log('Error listing users:', error);
        });
   };
-//   // Start listing users from the beginning, 1000 at a time.
-   listAllUsers();
 
-// getAuth()
-//   .getUser(uid)
-//   .then((userRecord) => {
-//     // See the UserRecord reference doc for the contents of userRecord.
-//     console.log(`Successfully fetched user data: ${userRecord.toJSON()}`);
-//   })
-//   .catch((error) => {
-//     console.log('Error fetching user data:', error);
-//   });
-
-
-// const listAllUsers = (nextPageToken) => {
-//     // List batch of users, 1000 at a time.
-//     getAuth()
-//       .listUsers(1000, nextPageToken)
-//       .then((listUsersResult) => {
-//         listUsersResult.users.forEach((userRecord) => {
-//           console.log('user', userRecord.toJSON());
-//         });
-//         if (listUsersResult.pageToken) {
-//           // List next batch of users.
-//           listAllUsers(listUsersResult.pageToken);
-//         }
-//       })
-//       .catch((error) => {
-//         console.log('Error listing users:', error);
-//       });
-//   };
-//   // Start listing users from the beginning, 1000 at a time.
-//   listAllUsers();
+listAllUsers();
