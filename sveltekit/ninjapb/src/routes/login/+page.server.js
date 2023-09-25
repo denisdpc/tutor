06fde21f9c8ef74e36a8a7d27244e0a1c8fefd58
denisdp <denisdpc@gmail.com>
2023-09-25 16:06:51 -0300

    hash login

--- sveltekit/ninjapb/src/routes/login/+page.server.js
+++ sveltekit/ninjapb/src/routes/login/+page.server.js
@@ -1,10 +1,17 @@
+import { SECRET_SIGNATURE } from "$env/static/private";
 import { redirect } from "@sveltejs/kit";
+import crypto from 'crypto';
 
 export const actions = {
     login: async({ request, locals }) => {
         const loginFormData = await request.formData();
         const email = loginFormData.get('email')??'';
         const password = loginFormData.get('password')??'';
+
+        const hash = crypto.createHash('sha256');
+        hash.update(password);
+        hash.update(SECRET_SIGNATURE);
+        const hashedPassword = hash.digest('hex');
 
         let loginResponse = {
             email,
@@ -13,7 +20,7 @@ export const actions = {
         }
 
         try {
-            await locals.userPb.collection('users').authWithPassword(email, password);
+            await locals.userPb.collection('users').authWithPassword(email, hashedPassword);
             loginResponse.error = locals.userPb.authStore.isValid
         } catch(err) {            
             console.log('ERRO login > +page.server.js: ');
